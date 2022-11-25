d038f3991a29de3e2963f93002c5c96f35b586ce
Mawserino <boholbryan25@gmail.com>
2022-11-25 20:23:30 +0800

    reactive button

--- app/src/main/java/com/example/pocketcard/activities/MainActivity.java
+++ app/src/main/java/com/example/pocketcard/activities/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     TextView register;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
